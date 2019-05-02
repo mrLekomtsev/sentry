8407547022d26d3e389a05552100b49cb4108597
Dan Fuller <dfuller@sentry.io>
2019-05-02 15:59:39 -0700

    feat(api): Add endpoint to return details of a specific incident. (SEN-520)
    This endpoint returns details about a specific incident. Same format as the list endpoint.

--- src/sentry/api/endpoints/organization_incident_index.py
+++ src/sentry/api/endpoints/organization_incident_index.py
@@ -1,24 +1,16 @@
 from __future__ import absolute_import
 
 from sentry import features
-from sentry.api.bases.organization import (
-    OrganizationEndpoint,
-    OrganizationPermission,
-)
+from sentry.api.bases.incident import IncidentPermission
+from sentry.api.bases.organization import OrganizationEndpoint
+from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.api.paginator import OffsetPaginator
 from sentry.api.serializers import serialize
 from sentry.incidents.models import Incident
 
 
-class OrganizationIncidentPermission(OrganizationPermission):
-    scope_map = {
-        'GET': ['org:read', 'org:write', 'org:admin'],
-        'POST': ['org:write', 'org:admin'],
-    }
-
-
 class OrganizationIncidentIndexEndpoint(OrganizationEndpoint):
-    permission_classes = (OrganizationIncidentPermission, )
+    permission_classes = (IncidentPermission, )
 
     def get(self, request, organization):
         """
@@ -29,7 +21,7 @@ class OrganizationIncidentIndexEndpoint(OrganizationEndpoint):
         :auth: required
         """
         if not features.has('organizations:incidents', organization, actor=request.user):
-            return self.respond(status=404)
+            raise ResourceDoesNotExist
 
         incidents = Incident.objects.fetch_for_organization(
             organization,
